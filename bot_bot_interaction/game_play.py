74d2ab337b8ef8371f7504fa0262c32306b0d56a
Kushal Chawla <kushalchawla18@gmail.com>
2023-02-21 15:32:48 -0800

    bot bot

--- bot_bot_interaction/game_play.py
+++ bot_bot_interaction/game_play.py
@@ -130,11 +130,90 @@ class GamePlay:
 
         return points
 
-    def get_all_deal_points(self, cxts):
-        pass
+    def get_feasible_deals(self, conv):
+        """Get a list of all feasible deals."""
+        
+        # first get the counts of the three items in this dialogue.
+        cxt = list(conv["cxts"].values())[0]
+        items = cxt.strip().split()
+        assert len(items) == 11
 
-    def check_pareto_optimal(self, all_deal_points, deal):
-        pass
+        i1_c, i2_c, i3_c = int(items[2].rstrip(",")), int(items[5].rstrip(",")), int(items[8].rstrip(","))
+        ccs = [i1_c, i2_c, i3_c]
+
+        issues = None
+        if "book" in cxt:
+            issues = ["book", "hat", "ball"]
+        else:
+            issues = ["food", "water", "firewood"]
+        
+        counts = {}
+        for iss, cc in zip(issues, ccs):
+            counts[iss] = cc
+
+        mnames = list(conv["cxts"].keys())
+        all_deals = []
+
+        for d1 in range(counts[issues[0]] + 1):
+            for d2 in range(counts[issues[1]] + 1):
+                for d3 in range(counts[issues[2]] + 1):
+                    new_deal = {
+                        mnames[0]: {
+                            issues[0]: d1,
+                            issues[1]: d2,
+                            issues[2]: d3,
+                        },
+                        mnames[1]: {
+                            issues[0]: counts[issues[0]] - d1,
+                            issues[1]: counts[issues[1]] - d2,
+                            issues[2]: counts[issues[2]] - d3,
+                        }
+                    }
+                    all_deals.append(new_deal)
+        
+        return all_deals
+
+    def get_deal_points(self, conv, deal):
+        """Get the points scored by both players according to the specific deal."""
+        
+        deal_s = {}
+
+        for mname in conv["cxts"].keys():
+            # get issue-wise pref values
+            prefs = self.get_pref_values(conv["cxts"][mname])
+
+            # get the points scored
+            points = self.compute_points_scored(prefs, deal, mname)
+
+            deal_s[mname] = points
+
+        return deal_s
+
+    def check_pareto_optimal(self, conv, deal):
+        """Check if the agreed deal is pareto optimal.
+        
+        A deal is pareto optimal if the score of one cannot be improved any further without lowering the partner's score."""
+        mnames = list(conv["cxts"].keys())
+
+        feasible_deals = self.get_feasible_deals(conv)
+
+        deal_s = self.get_deal_points(conv, deal)
+        s1, s2 = deal_s[mnames[0]], deal_s[mnames[1]]
+
+        can_improve = False
+        for cand_deal in feasible_deals:
+            cand_deal_s = self.get_deal_points(conv, cand_deal)
+            cs1, cs2 = cand_deal_s[mnames[0]], cand_deal_s[mnames[1]]
+
+            if (cs1 > s1 and cs2 >= s2) or (cs2 > s2 and cs1 >= s1):
+                can_improve = True
+                break
+
+        if can_improve:
+            # i.e. not optimal.
+            return False
+        
+        return True
 
     def get_conv_results(self, conv):
         """Compute the results from a single conv.
@@ -185,7 +264,7 @@ class GamePlay:
                 prefs = self.get_pref_values(conv["cxts"][mname])
 
                 # get the points scored
-                points = self.compute_points_scored(prefs, deal,mname)
+                points = self.compute_points_scored(prefs, deal, mname)
 
                 # save
                 results["per_model"][mname]["points"]  = points
@@ -201,12 +280,10 @@ class GamePlay:
                 joint_points += results["per_model"][mname]["points"]
             results["joint"]["joint_points"] = joint_points
             
-            all_deal_points = self.get_all_deal_points(conv["cxts"])
-            
             # just like here: https://github.com/facebookresearch/end-to-end-negotiator/blob/bbb93bbf00f69fced75d5c0d22e855bda07c9b78/src/eval_selfplay.py#L117
-            is_pareto_optimal = self.check_pareto_optimal(all_deal_points, deal)
+            is_pareto_optimal = self.check_pareto_optimal(conv, deal)
             
-            results["joint"]["pareto_optimal"] = is_pareto_optimal
+            results["joint"]["pareto_optimal"] = int(is_pareto_optimal)
         else:
             results["joint"]["deal_detected"] = 0
 
